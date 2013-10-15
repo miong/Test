147ef412aec6e29b30290e644d3c821caeea21e3
miong <mion.ggb@gmail.com>
2013-10-15 11:27:32 +0200

    failing test 1

---
+++ TP1_mion_carpentier/Url_test.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Url_test {
+
+	private UrlSplitter us;
+	
+	@Before
+	public void setUp(){
+		us = new UrlSplitter();
+	}
+	
+	@After
+	public void tearDown(){
+		us = null;
+	}
+	
+	@Test
+	public void testProtocol() {
+		us.split("http://some.thing");
+		assertEquals("http",us.getProtocol());
+	}
+	
+	@Test
+	public void testDomain(){
+		us.split("ftp://a.large.site");
+		assertEquals("a.large.site",us.getDomain());
+	}
+	
+	@Test
+	public void testPath(){
+		us.split("http://a.site.with/a-path");
+		assertEquals("a-path",us.getPath());
+	}
+	
+}
