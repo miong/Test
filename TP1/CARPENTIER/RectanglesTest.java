daa46991e9c01afd54f00ec0e6b8d1ca4398e518
kennypancarte <kenny.pancarte@gmail.com>
2013-10-25 16:51:34 +0200

    testFail1
    test Constructeur

---
+++ TP1/CARPENTIER/RectanglesTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RectanglesTest {
+
+	
+	
+	@Test
+	//test constructeur
+	public void RectangleTest(){
+		RectangleT rValide=new RectangleT(0,4,0,5);
+		assertEquals(rValide.xmin,0);
+		assertEquals(rValide.xmax,4);
+		assertEquals(rValide.ymin,0);
+		assertEquals(rValide.ymax,5);
+		
+		RectangleT rInvalide= new RectangleT(4,0,0,5);
+		assertEquals(rInvalide.xmin,0);
+		assertEquals(rInvalide.xmax,4);
+		assertEquals(rInvalide.ymin,0);
+		assertEquals(rInvalide.ymax,5);
+	}
+	
+	
+	//test aire rectangle
+	
+	//test perimetre rectangle
+	//test overlaping 
+}
