a65923bf6636e492aa5a7e01469aa26c5d782242
kennypancarte <kenny.pancarte@gmail.com>
2013-10-25 17:10:38 +0200

    TestFail2
    test aire

--- TP1/CARPENTIER/RectanglesTest.java
+++ TP1/CARPENTIER/RectanglesTest.java
@@ -1,14 +1,33 @@
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RectanglesTest {
-
 	
+	RectangleT rValide;
+	RectangleT rInvalide;
+	RectangleT rNull;
+	
+	@Before 
+	public void setUp(){
+		rValide=new RectangleT(0,4,0,5);
+		rInvalide= new RectangleT(4,0,0,5);
+		rNull=new RectangleT(0,0,0,0);
+	}
+	
+	@After
+	public void tearDown(){
+		rValide=null;
+		rInvalide=null;
+		rNull=null;
+	}
 	
 	@Test
 	//test constructeur
 	public void RectangleTest(){
-		RectangleT rValide=new RectangleT(0,4,0,5);
+		
 		assertEquals(rValide.xmin,0);
 		assertEquals(rValide.xmax,4);
 		assertEquals(rValide.ymin,0);
@@ -23,6 +42,12 @@ public class RectanglesTest {
 	
 	
 	//test aire rectangle
+	@Test
+	public void AirTest(){
+		assertEquals(20,rValide.aire());
+		assertEquals(20,rInvalide.aire());
+		assertEquals(0,rNull.aire());
+	}
 	
 	//test perimetre rectangle
 	//test overlaping 
