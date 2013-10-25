e5b7399953b1305c64ca8645f580db7b9443ef39
kennypancarte <kenny.pancarte@gmail.com>
2013-10-25 22:13:22 +0200

    TestSucess2
    intersecte

--- TP1/PANCARTE/sujet2/Line.java
+++ TP1/PANCARTE/sujet2/Line.java
@@ -14,5 +14,33 @@ public class Line {
 	}
 	
 	
-	
+	public boolean intersecte(Line l){
+		double precision= 0.000001;
+		
+		int abscisseVecteurX1= this.x2-this.x1;
+		int ordonneeVecteurY1= this.y2-this.y1;
+		
+		int abscisseVecteurX2= l.x2-l.x1;
+		int ordonneeVecteurY2= l.y2-l.y1;
+		
+		double rapportVecteurX=(double)abscisseVecteurX2/(double)abscisseVecteurX1;
+		double rapportVecteurY=(double)ordonneeVecteurY2/(double)ordonneeVecteurY1;
+		
+		if(  ((rapportVecteurX-rapportVecteurY)<precision) && ((rapportVecteurX-rapportVecteurY)>-precision)  ){
+			//les vecteurs directeurs des droites sont paralleles
+			int abscisseVecteurX3= this.x2-l.x1;
+			int ordonneeVecteurY3= this.y2-l.y1;
+			double rapportVecteurY3=(double)ordonneeVecteurY3/(double)ordonneeVecteurY1;
+			double rapportVecteurX3=(double)abscisseVecteurX3/(double)abscisseVecteurX1;
+			
+			if(  ((rapportVecteurX3-rapportVecteurY3)<precision) && ((rapportVecteurX3-rapportVecteurY3)>-precision)  ){
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return true;
+		
+	}
 }
